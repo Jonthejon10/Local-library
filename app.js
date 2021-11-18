b18659c99cb0ee488d1a60518f682d1ac30111d8
Ionut Ianchis <i.ionutz50@gmail.com>
2021-11-18 21:56:25 +0200

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var app = express();
 app.use(helmet())
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB =
-  'mongodb+srv://m001-student:Mysuperpass7@cluster0.amcem.mongodb.net/local_library?retryWrites=true&w=majority'
-  
+var dev_db_url =
+	'mongodb+srv://m001-student:Mysuperpass7@cluster0.amcem.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
